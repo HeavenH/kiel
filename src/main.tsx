bc48d867508739af0f3eb792f39aa74f64e795da
gil <gilmar.filho@contaazul.com>
2022-09-01 12:20:10 -0300

    chore: add aws

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import {ApolloProvider} from "@apollo/client";
-import {client} from "./apollo";
+import { client } from "./apollo";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
