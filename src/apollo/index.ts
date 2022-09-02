469bc46f995ba6c5f3f8b83c21ee880daf4ca565
gil <gilmar.filho@contaazul.com>
2022-09-02 06:56:45 -0300

    chore: add url

--- src/apollo/index.ts
+++ src/apollo/index.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-  uri: 'http://localhost:3000/graphql',
+  uri: 'http://kiew-env.eba-vempvxdm.us-east-1.elasticbeanstalk.com/graphql',
   cache: new InMemoryCache(),
 });
